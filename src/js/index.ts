b7a2dc582450418c81d32fdd540e65e4cad4d3ed
Sho.ayb <Sho.ayb@outlook.com>
2024-10-09 18:36:21 +0100

    amended the comment

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,7 +4,7 @@
 import '../scss/style.scss';
 
 // importing the logo image
-// Nb. on getting the following ts error: cannot find module or its corresponding type declarations, the solution is to create a custom.d.ts file, which tells typescript to treat these image files with specific extensions with a type declaration of "any".
+// Nb. on getting the following ts error: cannot find module or its corresponding type declarations, the solution is to create a custom.d.ts file, which tells typescript to treat these image files with specific extensions with a type declaration of "string".
 import logoImg from '../assets/img/logo.png';
 
 // query selection of DOM elements
