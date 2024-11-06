0f28c33be18b4a4dce88d57e5e8b04714b3bffd8
Sho.ayb <Sho.ayb@outlook.com>
2024-11-06 01:54:58 +0000

    Updating latest updates to layout

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,23 +1,63 @@
-"use-strict";
+'use-strict';
 
 // importing the style file
-import "../scss/style.scss";
+import '../scss/style.scss';
 
 // importing the logo image
 // Nb. on getting the following ts error: cannot find module or its corresponding type declarations, the solution is to create a custom.d.ts file, which tells typescript to treat these image files with specific extensions with a type declaration of "string".
-import logoImg from "../assets/img/logo.png";
-import icons from "../assets/img/icons.svg";
+import logoImg from '../assets/img/logo.png';
 
 // query selection of DOM elements
 
-const headerEl = document.querySelector(".header")! as HTMLElement;
+const headerEl = document.querySelector('.header')! as HTMLElement;
 
 // creating a img element
 
-const logo = document.createElement("img") as HTMLImageElement;
+const logo = document.createElement('img') as HTMLImageElement;
 logo.src = logoImg;
-logo.alt = "Logo";
-logo.className = "header__logo";
+logo.alt = 'Logo';
+logo.className = 'header__logo';
 
 // insert the logo
-headerEl.insertAdjacentElement("afterbegin", logo);
+headerEl.insertAdjacentElement('afterbegin', logo);
+
+const init = () => {
+  function toggleSearchList() {
+    const aside = document.querySelector('.recipe__search-list-container');
+    aside?.classList.toggle('active');
+  }
+
+  // Call this on page load and window resize
+
+  function handleResponsiveLayout() {
+    const aside = document.querySelector('.recipe__search-list-container');
+    if (window.innerWidth >= 768) {
+      aside?.classList.remove('hidden');
+      aside?.classList.remove('active');
+    } else {
+      aside?.classList.add('hidden');
+    }
+  }
+
+  function handleSearch(event: Event) {
+    event.preventDefault();
+  }
+
+  window.addEventListener('resize', handleResponsiveLayout);
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const searchForm = document.querySelector('.header__search-form');
+    if (searchForm) {
+      searchForm.addEventListener('submit', handleSearch);
+    }
+
+    const searchBtn = document.querySelector('.header__search-form-btn');
+    if (searchBtn) {
+      searchBtn.addEventListener('click', toggleSearchList);
+    }
+
+    handleResponsiveLayout(); // call on page load
+  });
+};
+
+init();
